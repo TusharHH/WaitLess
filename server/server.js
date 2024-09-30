47616e6817963eafd69af21f457d3ed73e2225f8
Tushar <tushar.hasule@somaiya.edu>
2024-10-01 00:56:15 +0530

    backend completed!!

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const connection = require('./connection.js');
 const adminRoutes = require('./routes/Admin.routes.js');
 const serviceRoutes = require('./routes/Service.routes.js');
 const userRoutes = require('./routes/User.routes.js');
+const tokenRoutes = require('./routes/Token.route.js');
+const queueRoutes = require('./routes/Queue.routes.js');
 
 const app = express();
 dotenv.config();
@@ -16,9 +18,11 @@ connection();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/admins',adminRoutes);
-app.use('/api/v1/users',userRoutes);
-app.use('/api/v1/services',serviceRoutes);
+app.use('/api/v1/admins', adminRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/services', serviceRoutes);
+app.use('/api/v1/tokens', tokenRoutes);
+app.use('/api/v1/queues', queueRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
