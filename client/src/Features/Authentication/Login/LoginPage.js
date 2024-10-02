942a4778f16676ac923ba633b50de6445586e710
Tushar <tushar.hasule@somaiya.edu>
2024-10-02 15:55:50 +0530

    adsf

--- client/src/Features/Authentication/Login/LoginPage.js
+++ client/src/Features/Authentication/Login/LoginPage.js
@@ -30,7 +30,7 @@ const LoginPage = () => {
         console.log(success);
 
         if (success) {
-            navigate('/u_dashboard'); // Redirect to dashboard upon successful login
+            navigate('/dashboard'); // Redirect to dashboard upon successful login
         }else{
             console.log('loggin failed!!');
             
