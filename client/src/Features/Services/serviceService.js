1e9bc14f4404579bd667b33894964b42981807f7
suthakaranburaj <suthakaranburaj@gmail.com>
2024-10-02 17:34:58 +0530

    bbbb

--- client/src/Features/Services/serviceService.js
+++ client/src/Features/Services/serviceService.js
@@ -5,6 +5,7 @@ const BACKEND_URL = 'http://localhost:4000/api/v1/services';
 export const get_all_service = async () => {
     const token = localStorage.getItem('token');
     try {
+        console.log(token);
         const response = await axios.get(`${BACKEND_URL}/services`, {
             headers: {
                 Authorization: `Bearer ${token}`,
